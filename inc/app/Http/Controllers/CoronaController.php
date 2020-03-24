131139d8af9a5f01e1695c2787626feb2b72f344
Rabiul Hasan Riaz <34161750+rabiulhasanriaz@users.noreply.github.com>
2020-03-24 16:17:04 +0600

    Add files via upload

--- inc/app/Http/Controllers/CoronaController.php
+++ inc/app/Http/Controllers/CoronaController.php
@@ -9,6 +9,7 @@ class CoronaController extends Controller
     public function index(){
         $total = file_get_contents("https://www.worldometers.info/coronavirus/");
         
+        
         $get_todays_span_data_to_last = explode("<span class=\"number-table\"", $total)[1];
         $get_todays_span_data = explode("</span>", $get_todays_span_data_to_last)[0];
         $todays_span_mild = "<span ". $get_todays_span_data . "</span>";
@@ -48,5 +49,34 @@ class CoronaController extends Controller
     }
     public function prevention(){
         return view('prevention');
+    }
+    public function bangladesh(){
+        $total_bd = file_get_contents("https://www.iedcr.gov.bd/");
+
+        $get_todays_div_bd_data_to_last = explode("<div class=\"counter\"", $total_bd)[1];
+        $get_todays_div_bd_data = explode("</div>", $get_todays_div_bd_data_to_last)[0];
+        $todays_div_bd = "<div ". $get_todays_div_bd_data . "</div>";
+
+        $get_todays_div_bd_data_to_last_24 = explode("<div class=\"counter\"", $total_bd)[2];
+        $get_todays_div_bd_data_24 = explode("</div>", $get_todays_div_bd_data_to_last_24)[0];
+        $todays_div_bd_24 = "<div ". $get_todays_div_bd_data_24 . "</div>";
+
+        $get_todays_div_bd_data_to_last_confirm = explode("<div class=\"counter\"", $total_bd)[3];
+        $get_todays_div_bd_data_confirm = explode("</div>", $get_todays_div_bd_data_to_last_confirm)[0];
+        $todays_div_bd_confirm = "<div ". $get_todays_div_bd_data_confirm . "</div>";
+
+        $get_todays_div_bd_data_to_case_24 = explode("<div class=\"counter\"", $total_bd)[4];
+        $get_todays_div_bd_data_case_24 = explode("</div>", $get_todays_div_bd_data_to_case_24)[0];
+        $todays_div_bd_case_24 = "<div ". $get_todays_div_bd_data_case_24 . "</div>";
+
+        $get_todays_div_bd_data_to_rec = explode("<div class=\"counter\"", $total_bd)[5];
+        $get_todays_div_bd_data_rec = explode("</div>", $get_todays_div_bd_data_to_rec)[0];
+        $todays_div_bd_rec = "<div ". $get_todays_div_bd_data_rec . "</div>";
+
+        $get_todays_div_bd_data_to_death = explode("<div class=\"counter\"", $total_bd)[6];
+        $get_todays_div_bd_data_death = explode("</div>", $get_todays_div_bd_data_to_death)[0];
+        $todays_div_bd_death = "<div ". $get_todays_div_bd_data_death . "</div>";
+
+        return view('bangladesh',compact('todays_div_bd','todays_div_bd_24','todays_div_bd_confirm','todays_div_bd_case_24','todays_div_bd_rec','todays_div_bd_death'));
     }
 }
