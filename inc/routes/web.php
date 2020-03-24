29de1a760a820da4dce3ef7ba30d339bb7b95c3d
Rabiul Hasan Riaz <34161750+rabiulhasanriaz@users.noreply.github.com>
2020-03-24 16:18:26 +0600

    Add files via upload

--- inc/routes/web.php
+++ inc/routes/web.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/','CoronaController@index')->name('home');
 Route::get('world-wide','CoronaController@worldwide')->name('world-wide');
 Route::get('prevention','CoronaController@prevention')->name('prevention');
+Route::get('bangladesh','CoronaController@bangladesh')->name('bangladesh');
